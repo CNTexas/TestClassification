246601e25e2130239a2ece7acfced2e6fc9bc367
Texas <1172381257@qq.com>
2018-06-03 11:37:33 +0800

    修改SVM文件名

--- cross_valid.py
+++ cross_valid.py
@@ -17,8 +17,6 @@ def cross_validated():
     # 导入测试集
     testpath = "train_word_bag/tfdifspace.dat"
     test_set = readbunchobj(testpath)
-
-
     count=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
     list_k = {}
     for i in range(19):
